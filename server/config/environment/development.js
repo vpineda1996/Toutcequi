2847c0146a56e75edc41f5e6188a98524410a116
Chris Li <chris@city365.ca>
2016-09-23 20:31:50 -0700

    Added common URI for postgres db.

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -7,7 +7,7 @@ module.exports = {
 
   // Sequelize connection opions
   sequelize: {
-    uri: 'postgres://<username>:<password>@localhost:5432/hackathon'
+    uri: 'postgres://hackathon:hackathon@localhost:5432/hackathon2016'
   },
 
   // Seed database on startup
