fb4cfa26e048fccc6cbc42b1539927f995cda471
Jason <jason@vpineda1996.local>
2016-09-24 17:03:17 -0700

    added support for cross header ref

--- server/api/recipes/index.js
+++ server/api/recipes/index.js
@@ -5,6 +5,13 @@ var controller = require('./recipes.controller');
 
 var router = express.Router();
 
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
